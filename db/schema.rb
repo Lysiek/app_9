efcbac0b1646854405ba8c062b72040c55f43215
Lysiek <lysiekchhoun168@gmail.com>
2018-04-23 10:51:02 +0700

    chap7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420023512) do
+ActiveRecord::Schema.define(version: 20180423130526) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
