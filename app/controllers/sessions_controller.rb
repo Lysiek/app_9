7eec1ad86f2c593cc18b55d6ea15ee00e7378b40
Lysiek <lysiekchhoun168@gmail.com>
2018-05-08 08:35:06 +0700

    chap9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,27 @@ class SessionsController < ApplicationController
     user = User.find_by email: params_session[:email].downcase
 
     if user && user.authenticate(params_session[:password])
-      log_in user
-      redirect_to user
+      login_success user
     else
-      flash[:danger] = t "danger"
+      flash[:danger] = t ".danger"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def login_success user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_to user
+  end
+
+  def params_session
+    params[:session]
   end
 end
