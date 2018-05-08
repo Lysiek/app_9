d7743d5a1777529b3c0374863e327a0e6096fab6
Lysiek <lysiekchhoun168@gmail.com>
2018-05-08 08:35:06 +0700

    # This is a combination of 2 commits. # This is the 1st commit message:
    chap9
    
    # This is the commit message #2:
    
    chap10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,54 @@
 module SessionsHelper
-  def login_in(user)
+  def log_in user
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+  def remember user
+    user.remember
+    cookies_permanent.signed[:user_id] = user.id
+    cookies_permanent[:remember_token] = user.remember_token
   end
 
-  def logged_in
-    !current_user.nil?
+  def current_user
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by id: user_id
+    elsif cookies_id = cookies.signed[:user_id]
+      user = User.find_by id: cookies_id
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def log_out
+    forget current_user
+    session.delete :user_id
+    @current_user = nil
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  private
+
+  def cookies_permanent
+    cookies.permanent
   end
 end
