b331a761d551ab2d66767bef0714df0b19790e2d
Lysiek <lysiekchhoun168@gmail.com>
2018-05-16 15:41:24 +0700

    chap10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,54 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  validates :name, presence: true, length: {maximum: Settings.name.maximum}
-  before_save :email_downcase
+  attr_reader :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.password.minimum}, allow_nil: true
+
+  before_save :email_downcase
+
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.password.minimum}
 
   class << self
-    def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
   def remember
-    self.remember_token = User.new_token
-    update_attribute :remember_digest, User.digest(remember_token)
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  def authenticated? remember_token
+    return false if remember_digest.blank?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
-    update_attribute(:remember_digest, nil)
+    update_attributes remember_digest: nil
+  end
+
+  def current_user? user
+    self == user
   end
 
   private
+
   def email_downcase
     email.downcase!
   end
